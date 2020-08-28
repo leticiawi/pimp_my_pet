78e43b6992e5918652ed49bc336d177e9346419e
leticia wiermann <leticia.wiermann@gmail.com>
2020-08-28 16:12:16 -0300

    api fixing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   def create
     product = Product.find(params[:product_id])
-    order  = Order.create!(product: product, product_sku: product.sku, amount: product.price, state: 'pending', user: current_user)
+    order = Order.create!(product: product, product_sku: product.sku, amount: product.price, state: 'pending', user: current_user)
 
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
